dfda8d586b3ce66183c6bd679602f02b1018e71e
Viktors2 <vitjok22@gmail.com>
2023-05-17 19:01:31 +0200

    Symbol and cell click

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const board = document.querySelector(".board");
+const board = document.querySelector(".game_board");
 this.template = document.querySelector("#template_cell");
+const cells = document.querySelectorAll(".cell");
 
 for (let i = 0; i < 100; i++) {
   const newElement = this.template.content.firstElementChild.cloneNode(true);
   newElement.dataset.id = i;
   this.template.parentElement.append(newElement);
+}
+
+let symbol = "x";
+board.addEventListener("click", boardClickHandle);
+
+function boardClickHandle(event) {
+  if (!event.target.classList.contains("cell")) {
+    return;
+  }
+
+  clickHandle.bind(event.target)();
+}
+
+function clickHandle() {
+  if (this.textContent !== "") {
+    return;
+  }
+  this.textContent = symbol;
 }

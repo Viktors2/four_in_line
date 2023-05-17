129bdfd021b62176d5d4f774a24dd35e3e18b2ec
Viktors2 <vitjok22@gmail.com>
2023-05-17 18:35:05 +0200

    id added for every cell

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ this.template = document.querySelector("#template_cell");
 
 for (let i = 0; i < 100; i++) {
   const newElement = this.template.content.firstElementChild.cloneNode(true);
+  newElement.dataset.id = i;
   this.template.parentElement.append(newElement);
 }

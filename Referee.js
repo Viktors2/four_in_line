1e7f22df4e83f2b610bca3801e6861cca6569930
Viktors2 <vitjok22@gmail.com>
2023-05-19 20:08:44 +0200

    first part of combinations

--- Referee.js
+++ Referee.js
@@ -1,3 +1,38 @@
 class Referee {
-  checkWinner(moves, symbol, index, x, y) {}
+  checkWinner(moves, symbol, index) {
+    this.moves = moves;
+    const x = index % 10;
+    const y = (index - x) / 10;
+
+    if (
+      symbol == this.getSymbol(y, x + 1) &&
+      symbol == this.getSymbol(y, x + 2) &&
+      symbol == this.getSymbol(y, x + 3)
+    ) {
+      return true;
+    }
+
+    if (
+      symbol == this.getSymbol(y, x - 1) &&
+      symbol == this.getSymbol(y, x - 2) &&
+      symbol == this.getSymbol(y, x - 3)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  getSymbol(y, x) {
+    if (y < 0 || y > 9 || x < 0 || x > 9) {
+      return null;
+    }
+
+    const index = y * 10 + x;
+    if (!moves.hasOwnProperty(index)) {
+      return null;
+    }
+
+    return this.moves[index];
+  }
 }

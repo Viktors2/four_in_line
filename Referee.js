7d3abf67832f438924192b0bb07067895cf53dc4
Viktors2 <vitjok22@gmail.com>
2023-05-24 20:15:17 +0200

    edited

--- Referee.js
+++ Referee.js
@@ -1,42 +1,42 @@
 class Referee {
   constructor(size = 10) {
     this.size = size;
-  }
 
-  checkWinner(moves, index) {
-    let match_count;
-    this.moves = moves;
-    const symbol = moves[index],
-      x = index % this.size,
-      y = Math.floor(index / this.size);
     const STAY = 0,
       LEFT = -1,
       RIGHT = 1,
       UP = -1,
       DOWN = 1;
-    horizontal: {
-      match_count = 0;
-      match_count += this.countMatchesOneDirection(symbol, y, x, STAY, RIGHT);
-      match_count += this.countMatchesOneDirection(symbol, y, x, STAY, LEFT);
 
-      if (match_count >= 3) {
-        return true;
-      }
-    }
-    vertical: {
-      match_count = 0;
-      match_count += this.countMatchesOneDirection(symbol, y, x, DOWN, STAY);
+    this.all_lines = [
+      /* Horizontal line */
+      [
+        [STAY, RIGHT],
+        [STAY, LEFT],
+      ],
+      /* Vertical line */
+      [[DOWN, STAY]],
+      /* dioganal_up_right line */
+      [
+        [UP, RIGHT],
+        [DOWN, LEFT],
+      ],
+      /* dioganal_down_right line */
+      [
+        [DOWN, RIGHT],
+        [UP, LEFT],
+      ],
+    ];
+  }
 
-      if (match_count >= 3) {
-        return true;
-      }
-    }
-    diagonal: {
-      match_count = 0;
-      match_count += this.countMatchesOneDirection(symbol, y, x, DOWN, RIGHT);
-      match_count += this.countMatchesOneDirection(symbol, y, x, DOWN, LEFT);
+  checkWinner(moves, index) {
+    this.moves = moves;
+    const symbol = moves[index],
+      x = index % this.size,
+      y = (index - x) / this.size;
 
-      if (match_count >= 3) {
+    for (const line of this.all_lines) {
+      if (this.checkLine(symbol, y, x, line)) {
         return true;
       }
     }
@@ -44,16 +44,24 @@ class Referee {
     return false;
   }
 
-  countMatchesOneDirection(symbol, y, x, direction_y, direction_x) {
+  checkLine(symbol, y, x, line) {
     let match_count = 0;
-    let currentY = y;
-    let currentX = x;
+    for (const vector of line) {
+      match_count += this.countMatchesOneDirection(symbol, y, x, vector);
+    }
 
+    if (match_count >= 3) {
+      return true;
+    }
+    return false;
+  }
+
+  countMatchesOneDirection(symbol, y, x, vector) {
+    let match_count = 0;
     for (let step = 1; step <= 3; step++) {
-      currentY += direction_y;
-      currentX += direction_x;
-
-      if (symbol !== this.getSymbol(currentY, currentX)) {
+      y += vector[0];
+      x += vector[1];
+      if (symbol != this.getSymbol(y, x)) {
         break;
       }
 
@@ -64,12 +72,12 @@ class Referee {
   }
 
   getSymbol(y, x) {
-    if (y < 0 || y >= this.size || x < 0 || x >= this.size) {
+    if (y < 0 || y > this.size - 1 || x < 0 || x > this.size - 1) {
       return null;
     }
 
     const index = y * this.size + x;
-    if (!this.moves.hasOwnProperty(index)) {
+    if (!moves.hasOwnProperty(index)) {
       return null;
     }
 

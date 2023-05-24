74c21f375689b6ef2dc62d69c812c54fd60e2cf7
Viktors2 <vitjok22@gmail.com>
2023-05-24 19:09:58 +0200

    variations of combinations

--- Referee.js
+++ Referee.js
@@ -1,67 +1,3 @@
-// class Referee {
-//   constructor() {
-//     this.size = 10;
-//   }
-
-//   checkWinner(moves, index) {
-//     const symbol = moves[index],
-//       x = index % this.size,
-//       y = Math.floor(index / this.size);
-
-//     const directions = [
-//       [1, 0], // Right
-//       [1, 1], // Diagonal right-down
-//       [0, 1], // Down
-//       [-1, 1], // Diagonal left-down
-//     ];
-
-//     for (const [coordX, coordY] of directions) {
-//       if (
-//         this.checkLine(symbol, x, y, coordX, coordY, moves) +
-//           this.checkLine(symbol, x, y, -coordX, -coordY, moves) >=
-//         3
-//       ) {
-//         return true;
-//       }
-//     }
-
-//     return false;
-//   }
-
-//   checkLine(symbol, x, y, coordX, coordY, moves) {
-//     let count = 0,
-//       newX = x + coordX,
-//       newY = y + coordY;
-
-//     while (
-//       newX >= 0 &&
-//       newX < this.size &&
-//       newY >= 0 &&
-//       newY < this.size &&
-//       symbol === this.getSymbol(newX, newY, moves)
-//     ) {
-//       count++;
-//       newX += coordX;
-//       newY += coordY;
-//     }
-
-//     return count;
-//   }
-
-//   getSymbol(x, y, moves) {
-//     if (x < 0 || x >= this.size || y < 0 || y >= this.size) {
-//       return null;
-//     }
-
-//     const index = y * this.size + x;
-//     if (!moves.hasOwnProperty(index)) {
-//       return null;
-//     }
-
-//     return moves[index];
-//   }
-// }
-
 class Referee {
   constructor(size = 10) {
     this.size = size;
@@ -97,15 +33,7 @@ class Referee {
     }
     diagonal: {
       match_count = 0;
-      match_count += this.countMatchesOneDirection(symbol, y, x, UP, RIGHT);
       match_count += this.countMatchesOneDirection(symbol, y, x, DOWN, RIGHT);
-
-      if (match_count >= 3) {
-        return true;
-      }
-
-      match_count = 0;
-      match_count += this.countMatchesOneDirection(symbol, y, x, UP, LEFT);
       match_count += this.countMatchesOneDirection(symbol, y, x, DOWN, LEFT);
 
       if (match_count >= 3) {

2522aaaa5a0fb9c4c8de84985efd21528d4d6fb1
Viktors2 <vitjok22@gmail.com>
2023-05-26 18:10:27 +0200

    localstorage added

---
+++ Storage.js
@@ -0,0 +1,86 @@
+class Storage {
+  /**
+   *
+   * @param {string} storage_name - custom name of the current storage
+   */
+  constructor(storage_name) {
+    this.storage_name = storage_name;
+
+    this.data = {
+      entries: {},
+    };
+
+    const json_data = localStorage.getItem(this.storage_name);
+    if (json_data == null) {
+      return;
+    }
+
+    this.data = JSON.parse(json_data);
+  }
+
+  getEntries() {
+    return this.data.entries;
+  }
+
+  add(index, entry) {
+    this.data.entries[index] = {};
+    for (const key in entry) {
+      this.data.entries[index][key] = entry[key];
+    }
+
+    this.save();
+  }
+
+  /**
+   *
+   * @param {*} id
+   * @param {*} data - Example {time: "10:12"} or {status: 1}
+   */
+  update(index, data) {
+    for (const key in data) {
+      this.data.entries[index][key] = data[key];
+    }
+
+    this.save();
+  }
+
+  delete(index) {
+    delete this.data.entries[index];
+    this.save();
+  }
+
+  save() {
+    const json_data = JSON.stringify(this.data);
+    localStorage.setItem(this.storage_name, json_data);
+  }
+  clearFunct() {
+    // localStorage.clear();
+    // or
+    this.data.entries = {};
+    this.save();
+  }
+}
+
+// https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/
+// {
+//   "status": true,
+//   "0": "https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/{storage_name}/?action=create",
+//   "1": "https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/{storage_name}/?action=get-all",
+//   "2": "https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/{storage_name}/?action=update",
+//   "3": "https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/{storage_name}/?action=delete",
+//   "4": "https://bezkomisijas.lv/api-storage/1afa146fca636c3026b1804c03cd7266/{storage_name}/?action=clear"
+// }
+
+// const json = `{
+//   "firstname": "Leo",
+//   "lastname": "Ozoliņš",
+//   "age": 45,
+//   "skils": ["html", "css", "js"]
+// }`;
+
+// const obj = {
+//   firstname: "Leo",
+//   lastname: "Ozoliņš",
+//   age: 45,
+//   skils: ["html", "css", "js"]
+// };
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const board = document.querySelector(".game_board");
 this.template = document.querySelector("#template_cell");
 const cells = document.querySelectorAll(".cell");
+const popup = document.querySelector('.popup');
+const message_el = popup.querySelector('.message');
+
+
 let index = 0;
 
 for (let y = 0; y < 10; y++) {
@@ -16,16 +20,22 @@ for (let y = 0; y < 10; y++) {
   }
 }
 
-let i = 82;
-board.children[i].style.background = "lightgreen";
-board.children[i + 10].style.background = "lightyellow";
-board.children[i + 1].style.background = "lightblue";
-board.children[i - 1].style.background = "lightpink";
-
 let count = 0;
-
 let referee = new Referee();
-let moves = [];
+let storage = new Storage("fourinline");
+let entries = storage.getEntries();
+let moves = {};
+
+console.log(entries);
+
+for (const index in entries) {
+  const entry = entries[index];
+  const symbol = entry.symbol;
+  const cell = board.children[index];
+  cell.textContent = symbol;
+  moves[index] = symbol;
+}
+
 
 // where players can put their symbols (board.children)
 // clickedHandle function
@@ -41,6 +51,7 @@ function clickHandle() {
   const index = Number(this.dataset.index);
   const x = this.dataset.x;
   const y = this.dataset.y;
+
   if (moves.hasOwnProperty(index)) {
     return;
   }
@@ -49,7 +60,9 @@ function clickHandle() {
   }
   const symbol = ++count % 2 == 0 ? "o" : "x";
   moves[index] = symbol;
-  this.textContent = symbol;
+  this.textContent = symbol; 
+   storage.add(index, { symbol: symbol });
+
   console.log(
     "symbol: " +
       symbol +
@@ -62,10 +75,29 @@ function clickHandle() {
       this.dataset.index
   );
 
-  if (referee.checkWinner(moves, index, x, y)) {
-    console.log("Player " + moves[index] + " has won the game!");
+  if (referee.checkWinner(moves, index)) {
+    showMessage("Player " + symbol + ' has won the game!');
   }
 }
+
+document.querySelector(".reset").addEventListener("click", resetHandle);
+document.querySelector(".reset_game").addEventListener("click", resetHandle);
+
+popup.addEventListener('click', function (event) {
+  if (event.target.classList.contains('popup')) {
+    hideMessage();
+  }
+})
+
+function showMessage(message) {
+  popup.classList.add('open');
+  message_el.textContent = message;
+}
+
+function hideMessage() {
+  popup.classList.remove('open');
+}
+
 
 const reset = document.querySelector(".reset");
 reset.addEventListener("click", resetHandle);
@@ -78,4 +110,8 @@ function resetHandle() {
   }
   count = "0";
   moves = {};
+  storage.clearFunct();
+  hideMessage();
+
 }
+
